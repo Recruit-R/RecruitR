ff05fa0306ccabfb0ba38b16a8431f9a5a232332
freycp20 <freycp20@gcc.edu>
2023-12-15 12:31:45 -0500

    fixed form validation date obj

--- app/recruiter/events-demo/actions.ts
+++ app/recruiter/events-demo/actions.ts
@@ -10,9 +10,14 @@ const formSchema = z.object({
 export async function create(value: string) {
     // ...
     const obj = JSON.parse(value);
-    console.log("test")
+    console.log(obj)
+
     const res = await addData("events", "2", {"Title": "asdf", "Location": "GCC", "Time": { "seconds": 1702498664, "nanoseconds": 569000000 }})
     console.log("IT WORKED")
     console.log(res.result)
 
+}
+function convert(seconds, nanoseconds) {
+    // Create a Date object from the seconds and nanoseconds.
+    return new Date(seconds * 1000 + nanoseconds / 1000000);
 }
