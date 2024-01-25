adf49bcea8fe168846bd0c8404c8ac4a8318d362
TheDeafOne <Woodburnkb20@gcc.edu>
2024-01-24 22:49:07 -0500

    move general getuser logic to components

--- app/candidate/home/page.tsx
+++ app/candidate/home/page.tsx
@@ -1,24 +1,8 @@
-import { auth } from "@/firebase/server";
 import { DecodedIdToken } from "firebase-admin/auth";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+import { getUser } from "@/components/get-user";
 import SignOut from "../components/signout-button";
-
-async function getUser() {
-    const cookieStore = cookies();
-    const authToken = cookieStore.get("firebaseIdToken")?.value;
-    let user: DecodedIdToken | null = null;
-    try {
-        if (auth && authToken) {
-            user = await auth.verifyIdToken(authToken);
-        }
-    } catch (error) {
-        // One possible error is the token being expired, return forbidden
-        console.log(error);
-    }
-    return user
-}
 
 export default async function CandidateHome() {
     const user: DecodedIdToken | null = await getUser();
