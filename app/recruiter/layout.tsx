18e5d228933a8a49a92068e9ff8776c45512517c
TheDeafOne <Woodburnkb20@gcc.edu>
2024-01-24 22:49:30 -0500

    recruiter layout redirect to auth page when user not authenticated

--- app/recruiter/layout.tsx
+++ app/recruiter/layout.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import NavBar from "@/components/nav-bar";
+import { DecodedIdToken } from "firebase-admin/auth";
+import { getUser } from "@/components/get-user";
+import { redirect } from "next/navigation";
 
-export default function RecruiterLayout({
+export default async function RecruiterLayout({
     children, // will be a page or nested layout
 }: {
     children: React.ReactNode
 }) {
+
+    const user: DecodedIdToken | null = await getUser();
+    if (user === null) {
+        return redirect("/auth/login");
+    } else if (user.role === "recruiter" || user.role === "coordinator") {
+        return redirect("/recruiter/home");
+    }
     return (
         <section className="flex h-screen flex-col grow-0 overflow-y-hidden">
-            <div className="flex-0 px-4 border-y"><NavBar/></div>
+            <div className="flex-0 px-4 border-y"><NavBar /></div>
             <div className="flex-1 overflow-y-hidden">
                 {children}
             </div>
