467368f77af89ef2d8a746ea82ba4e9015e52d28
TheDeafOne <Woodburnkb20@gcc.edu>
2024-01-29 16:48:16 -0500

    get rid of evil logging

--- app/recruiter/home/page.tsx
+++ app/recruiter/home/page.tsx
@@ -21,7 +21,6 @@ async function getStudents() {
 export default async function Page() {
     // const students = await getStudents()
     const new_students = await getData({ collection_name: 'users' })
-    console.log(new_students);
     return (
         <ClientComponent students={new_students} />
     )
