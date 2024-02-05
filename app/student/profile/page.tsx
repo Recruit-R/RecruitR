cd808ee1bc2405f3a65d94349c7a1a9add9e20e5
TheDeafOne <Woodburnkb20@gcc.edu>
2024-02-05 18:17:39 -0500

    remove get unused getUsers

--- app/student/profile/page.tsx
+++ app/student/profile/page.tsx
@@ -1,22 +1,7 @@
 import ClientComponent from "@/app/student/profile/components/client-component";
-import { studentSchema } from "@/app/recruiter/home/data/student-schema";
-import { promises as fs } from "fs";
-import path from "path";
-import { z } from "zod";
 import getData from "../../api/getData";
 
 
-
-
-async function getStudents() {
-    const data = await fs.readFile(
-        path.join(process.cwd(), "app/recruiter/home/data/student_data.json")
-    )
-
-    const tasks = JSON.parse(data.toString())
-
-    return z.array(studentSchema).parse(tasks)
-}
 
 export default async function Page() {
     // const students = await getStudents()
