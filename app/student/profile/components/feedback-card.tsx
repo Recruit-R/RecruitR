900943752e916b3adbdc35d97c889a50d559b0a6
TheDeafOne <Woodburnkb20@gcc.edu>
2024-02-05 19:59:34 -0500

    remove unused variable/function references/params

--- app/student/profile/components/feedback-card.tsx
+++ app/student/profile/components/feedback-card.tsx
@@ -1,32 +1,18 @@
 "use client"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
-import {Label} from "@/components/ui/label";
-import {Checkbox} from "@/components/ui/checkbox";
-import Link from "next/link";
-import {ChevronLeft} from "lucide-react";
-import {Button} from "@/components/ui/button";
-import React, {useEffect} from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { useEffect } from "react";
 //import {set} from "yaml/dist/schema/yaml-1.1/set";
-import {PossiblePlacement} from "@/app/student/profile/components/possible-placement";
-import {Textarea} from "@/components/ui/textarea";
-import {cn} from "@/lib/utils";
-import {useAmp} from "next/amp";
-import { StudentInfo } from "./student-info";
-import { ResumeButton } from "./resume-file";
-import { ShowSkills } from "./show-skills";
+import { PossiblePlacement } from "@/app/student/profile/components/possible-placement";
+import { cn as c } from "@/lib/utils";
 import { EditButton } from "./edit-button";
+import { ResumeButton } from "./resume-file";
+import { StudentInfo } from "./student-info";
 
-interface FeedbackCardProps {
-    feedbackFocus: boolean,
-    c: (classnames: string, conditionalNames: string, condition?: boolean) => string
-}
-
-export function FeedbackCard({feedbackFocus, c} : FeedbackCardProps) {
+export function FeedbackCard() {
     const languages: Array<String> = ["Python", "Java", "Kotlin", "R", "Angular", ".NET", "Canva", "Adobe Photoshop", "Agile Philosophy", "Power BI", "Azure DevOps", "Waterfall Methodologies"]
 
-    useEffect(() => {console.log(`RAHHH`)})
+    useEffect(() => { console.log(`RAHHH`) })
 
     return (
         <>
@@ -34,16 +20,16 @@ export function FeedbackCard({feedbackFocus, c} : FeedbackCardProps) {
                 <CardHeader className="flex flex-row items-center divide-x border-b mb-4">
                     <div className="flex flex-1 items-center space-x-4 pr-4">
                         <Avatar className="h-20 w-20">
-                            <AvatarImage src="/avatars/01.png" alt="Avatar"/>
+                            <AvatarImage src="/avatars/01.png" alt="Avatar" />
                             <AvatarFallback className="text-3xl">JM</AvatarFallback>
                         </Avatar>
                         <div>
-                                <CardTitle className="text-4xl">
-                                    Joe Mama
-                                </CardTitle>
-                                <CardDescription className="text-md">
-                                    Exercise Science Major
-                                </CardDescription>
+                            <CardTitle className="text-4xl">
+                                Joe Mama
+                            </CardTitle>
+                            <CardDescription className="text-md">
+                                Exercise Science Major
+                            </CardDescription>
 
                         </div>
                     </div>
@@ -55,9 +41,9 @@ export function FeedbackCard({feedbackFocus, c} : FeedbackCardProps) {
                     </div>
                 </CardHeader>
                 <CardContent className={c("flex flex-col flex-wrap", "xl:grid xl:grid-cols-2 xl:gap-x-4")}>
-                {/*    Initial feedback */}
+                    {/*    Initial feedback */}
                     <div className="flex flex-col gap-6">
-                        <div className={c("flex","xl:hidden")}>
+                        <div className={c("flex", "xl:hidden")}>
                         </div>
                         <div>
                             <p className="font-bold text-lg">
@@ -65,7 +51,7 @@ export function FeedbackCard({feedbackFocus, c} : FeedbackCardProps) {
                             </p>
                             <StudentInfo func={c}></StudentInfo>
                         </div>
-                        <PossiblePlacement/>
+                        <PossiblePlacement />
                         {/* <div className="space-y-1">
                             <p className="font-bold text-lg">
                                 Skills
@@ -82,11 +68,11 @@ export function FeedbackCard({feedbackFocus, c} : FeedbackCardProps) {
                             <ResumeButton />
                         </div>
                     </div>
-                    
+
                 </CardContent>
-                
+
             </Card>
-            
+
         </>
 
     )
--- app/student/profile/page.tsx
+++ app/student/profile/page.tsx
@@ -5,9 +5,7 @@ import getData from "../../api/getData";
 
 export default async function Page() {
     // const students = await getStudents()
-    const new_students = await getData({ collection_name: 'users' })
-    console.log(new_students);
     return (
-        <ClientComponent students={new_students} />
+        <ClientComponent />
     )
 }
