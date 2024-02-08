3a2e053f5b169ae7bd8d4862f646d9bb286c0fdb
TheDeafOne <Woodburnkb20@gcc.edu>
2024-02-08 15:30:33 -0500

    remove client side rerouting

--- app/candidate/home/page.tsx
+++ app/candidate/home/page.tsx
@@ -1,20 +1,6 @@
-'use client'
-import { useAuth } from "@/components/auth-provider";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import SignOut from "../components/signout-button";
 
 export default function CandidateHome() {
-    const router = useRouter();
-    const auth = useAuth();
-    useEffect(() => {
-        if (!auth?.currentUser) {
-            router.push("/auth/login");
-        } else if (auth?.isCoordinator || auth?.isRecruiter) {
-            router.push("/recruiter/home");
-        }
-    }, []);
-
     return (
         <div>
             Candidate Home
