7af7efe0728e2c726b9d70d65c32b59f86629f15
TheDeafOne <Woodburnkb20@gcc.edu>
2024-02-12 18:12:13 -0500

    format and clean

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -27,7 +27,7 @@ export default async function LoginPage() {
             if (user && (user.role === Roles.RECRUITER || user.role === Roles.COORDINATOR)) {
                 return redirect("/recruiter/home");
             } else {
-                return redirect("/candidate/home");
+                return redirect("/candidate/profile");
             }
         }
     }
--- app/candidate/profile/components/studentinfo-card.tsx
+++ app/candidate/profile/components/studentinfo-card.tsx
@@ -1,27 +1,17 @@
 "use client"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
-import {RadioGroup, RadioGroupItem} from "@/components/ui/radio-group";
-import {Label} from "@/components/ui/label";
-import {Checkbox} from "@/components/ui/checkbox";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-import {ChevronLeft} from "lucide-react";
-import {Button} from "@/components/ui/button";
-import React, {useEffect, useState} from "react";
+import React from "react";
 //import {set} from "yaml/dist/schema/yaml-1.1/set";
-import {PossiblePlacement} from "@/app/student/profile/components/possible-placement";
-import {Textarea} from "@/components/ui/textarea";
-import {cn} from "@/lib/utils";
-import {useAmp} from "next/amp";
-import { StudentInfo } from "./student-info";
-import { ResumeButton } from "./resume-file";
-import { ShowSkills } from "./show-skills";
-import { StatusBar } from "./status-bar";
-import { Dialog } from "@radix-ui/react-dialog";
+import { PossiblePlacement } from "@/app/candidate/profile/components/possible-placement";
 import { AboutMeDrawerDialog } from "./about-me-edit";
-import { DialogDemo } from "./about-me-dialog";
-import { PersonalDrawerDialog } from "./personal-edit";
 import { HeaderDrawerDialog } from "./header-edit";
+import { PersonalDrawerDialog } from "./personal-edit";
+import { ResumeButton } from "./resume-file";
+import { StatusBar } from "./status-bar";
+import { StudentInfo } from "./student-info";
 
 interface StudentInfoCardProps {
     editMode: boolean,
@@ -29,7 +19,7 @@ interface StudentInfoCardProps {
     c: (classnames: string, conditionalNames: string, condition?: boolean) => string
 }
 
-export function StudentInfoCard({editMode, setEditMode, c} : StudentInfoCardProps) {
+export function StudentInfoCard({ editMode, setEditMode, c }: StudentInfoCardProps) {
     /*const languages: Array<String> = ["Python", "Java", "Kotlin", "R", "Angular", ".NET", "Canva", "Adobe Photoshop", "Agile Philosophy", "Power BI", "Azure DevOps", "Waterfall Methodologies"]*/
     // useEffect(() => {console.log(`RAHHH`)})
     return (
@@ -37,23 +27,23 @@ export function StudentInfoCard({editMode, setEditMode, c} : StudentInfoCardProp
             <Card className="min-h-full ">
                 <CardHeader className="flex flex-row border-b mb-4">
                     <div className={`flex flex-row pr-3 items-center space-x-4 relative ${editMode && "animate-pulse"} rounded-lg`}>
-                            {
-                                editMode && 
-                                <HeaderDrawerDialog></HeaderDrawerDialog>
-                            }
-                            <Avatar className="h-20 w-20">
-                                <AvatarImage src="/avatars/01.png" alt="Avatar"/>
-                                <AvatarFallback className="text-3xl">JM</AvatarFallback>
-                            </Avatar>
-                            <div>
-                                <CardTitle className="text-4xl">
-                                    Joe Mama
-                                </CardTitle>
-                                <CardDescription className="text-md">
-                                    Exercise Science Major
-                                </CardDescription>
+                        {
+                            editMode &&
+                            <HeaderDrawerDialog></HeaderDrawerDialog>
+                        }
+                        <Avatar className="h-20 w-20">
+                            <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                            <AvatarFallback className="text-3xl">JM</AvatarFallback>
+                        </Avatar>
+                        <div>
+                            <CardTitle className="text-4xl">
+                                Joe Mama
+                            </CardTitle>
+                            <CardDescription className="text-md">
+                                Exercise Science Major
+                            </CardDescription>
 
-                            </div>
+                        </div>
                     </div>
                     <div className={`flex flex-col pl-3 ml-auto items-center`}>
                         <Button asChild variant={"link"}>
@@ -62,21 +52,21 @@ export function StudentInfoCard({editMode, setEditMode, c} : StudentInfoCardProp
                         <Button onClick={() => setEditMode(prevState => !prevState)}
                             variant={"outline"}
                             className="w-32"
-                            >
-                            {editMode ? "Cancel": "Edit Profile"}
+                        >
+                            {editMode ? "Cancel" : "Edit Profile"}
                         </Button>
                     </div>
                 </CardHeader>
                 <CardContent className={c("flex flex-col flex-wrap", "xl:grid xl:grid-cols-2 xl:gap-x-4")}>
-                {/*    Initial feedback */}
+                    {/*    Initial feedback */}
                     <div className="flex flex-col gap-6">
                         <div className={`relative ${editMode && "animate-pulse"} rounded-lg`}>
 
                             {
-                                editMode && 
+                                editMode &&
                                 <AboutMeDrawerDialog></AboutMeDrawerDialog>
                             }
-                            
+
                             <p className="font-bold text-lg">
                                 About Me
                             </p>
@@ -84,19 +74,19 @@ export function StudentInfoCard({editMode, setEditMode, c} : StudentInfoCardProp
                         </div>
                         <div className={`relative ${editMode && "animate-pulse"} rounded-lg`}>
                             {
-                                editMode && 
+                                editMode &&
                                 <PersonalDrawerDialog></PersonalDrawerDialog>
                             }
-                            
+
                             <p className="font-bold text-lg">
                                 Personal Info
                             </p>
                             <StudentInfo func={c}></StudentInfo>
                         </div>
-                        
-                        
 
-                        <PossiblePlacement/>
+
+
+                        <PossiblePlacement />
                         <StatusBar></StatusBar>
                         {/* <div className="space-y-1">
                             <p className="font-bold text-lg">
@@ -114,11 +104,11 @@ export function StudentInfoCard({editMode, setEditMode, c} : StudentInfoCardProp
                             <ResumeButton />
                         </div>
                     </div>
-                    
+
                 </CardContent>
-                
+
             </Card>
-            
+
         </>
 
     )
