03ed5eb3f7196b92ffb406f0a671ede81e6d9bb6
TheDeafOne <Woodburnkb20@gcc.edu>
2024-02-12 15:16:13 -0500

    try to fix manual token refresh

--- app/api/validateUser.ts
+++ app/api/validateUser.ts
@@ -1,9 +1,17 @@
 import { auth } from '@/firebase/server';
 
+
 export default async function validateUser(authToken: string | undefined) {
     if (auth && authToken) {
         try {
-            return await auth.verifyIdToken(authToken);
+            // return await auth.getIdToken(authToken);
+            try {
+                const user = await auth.verifyIdToken(authToken, true);
+            } catch (error) {
+                return null;
+            }
+
+
         } catch (error) {
             return null;
         }
