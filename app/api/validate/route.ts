b6896e81b61de2b43832a7ab2507f3525cfedf79
TheDeafOne <Woodburnkb20@gcc.edu>
2024-03-12 15:13:41 -0400

    more api and routing fuckery

--- app/api/validate/route.ts
+++ app/api/validate/route.ts
@@ -7,10 +7,10 @@ export async function GET(
 ) {
     try {
         const authToken: string | undefined = request.headers.get("authorization")!.split("Bearer ")[1];
-        if (authToken === null) return new NextResponse("Unauthorized", { status: 401 })
-        const user: DecodedIdToken | undefined | string = await validateUser(authToken) ?? undefined;
-        if (user === undefined) return new NextResponse("Unauthorized", { status: 405 });
-        if (user === "auth/id-token-expired") return new NextResponse("Refresh", { status: 302 });
+        // if (authToken === null) return new NextResponse("Unauthorized", { status: 401 })
+        const user: DecodedIdToken | undefined | string = await validateUser(authToken);// ?? undefined;
+        // if (user === undefined) return new NextResponse("Unauthorized", { status: 405 });
+        // if (user === "auth/id-token-expired") return new NextResponse("Refresh", { status: 302 });
         return NextResponse.json({ role: (user as DecodedIdToken).role }, { status: 200 });
     } catch (error) {
         return new NextResponse("Internal Error", { status: 500 });
