51a52ba90222525c15076262585ac6d34b18c28c
TheDeafOne <Woodburnkb20@gcc.edu>
2024-03-13 17:20:34 -0400

    check if user exists

--- app/api/reset_email/route.ts
+++ app/api/reset_email/route.ts
@@ -5,11 +5,15 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const email = body.email;
-    const user = await auth!.getUserByEmail(email);
-    if (user.providerData.some((userInfo) => userInfo.providerId === "password")) {
-        const resetLink = await auth!.generatePasswordResetLink(email);
-        sendEmail(email, "RecruitR Passowrd Reset", `Click the link to reset your password: <a href='${resetLink}' target='_blank'>Reset Password</a>`);
-    }
+    try {
+        const user = await auth!.getUserByEmail(email);
+        if (user.providerData.some((userInfo) => userInfo.providerId === "password")) {
+            const resetLink = await auth!.generatePasswordResetLink(email);
+            sendEmail(email, "RecruitR Passowrd Reset", `Click the link to reset your password: <a href='${resetLink}' target='_blank'>Reset Password</a>`);
+        }
 
-    return new NextResponse("Email Sent", { status: 200 });
+        return new NextResponse("Email Sent", { status: 200 });
+    } catch (error) {
+        return new NextResponse("User does not exist", { status: 404 });
+    }
 }
