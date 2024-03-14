24cc2677726d6674c2b3f5bf4d42db51721eb685
noYuen <yuennr20@gcc.edu>
2024-03-14 02:46:39 -0400

    Pulling possible placements and interview process

--- app/candidate/profile/components/possible-placement.tsx
+++ app/candidate/profile/components/possible-placement.tsx
@@ -8,9 +8,20 @@ export function PossiblePlacement({can_data}: {can_data:any}) {
             <p className="font-bold text-lg">
                 Possible Placement
             </p>
-            <p className="text-md">
+
+            <ul className="list-disc ml-5">
+                {can_data && Object.keys(can_data.feedback).map((da: any) => {
+                return (
+                    <li key = {da}>
+                        {can_data.feedback?.[da].possible_placement}
+                    </li>
+                )
+            })}
+            </ul>
+
+            {/* <p className="text-md">
                 <>{can_data && (can_data.feedback ? can_data.feedback.possible_placement : `N/A`)}</>
-            </p>
+            </p> */}
             
         </div>
     )
