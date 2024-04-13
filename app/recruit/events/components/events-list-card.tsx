d3e1f08b64f92e238a9c227aa24e8062731f0a31
TheDeafOne <Woodburnkb20@gcc.edu>
2024-04-13 14:34:48 -0400

    initialize states inside event map

--- app/recruit/events/components/events-list-card.tsx
+++ app/recruit/events/components/events-list-card.tsx
@@ -6,7 +6,7 @@ import {
     CardContent
 } from "@/components/ui/card";
 import Link from "next/link";
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import { MdOutlineQrCode2 } from "react-icons/md";
 import { EventDataContext, EventDataContextType } from "./client-component";
@@ -21,11 +21,10 @@ interface EventsListCardProps {
 }
 
 export function EventsListCard({ partialEvents, title, empty_message }: EventsListCardProps) {
-    const [deleteOpen, setDeleteOpen] = useState<boolean>(false);
-    const [editOpen, setEditOpen] = useState<boolean>(false);
+
     const { events, setEvents } = useContext(EventDataContext) as EventDataContextType
 
-    const DeleteConfirmationForm = ({ event }: { event: Event }) => {
+    const DeleteConfirmationForm = ({ event, setDeleteOpen }: { event: Event, setDeleteOpen: React.Dispatch<React.SetStateAction<boolean>> }) => {
         return (
             <div className="flex items-center justify-center">
                 <Button variant="destructive" onClick={() => {
@@ -54,15 +53,17 @@ export function EventsListCard({ partialEvents, title, empty_message }: EventsLi
                         partialEvents.length ?
                             partialEvents.map((event: Event, i: number) => {
                                 const date = event.date.toDateString() + " " + event.date!.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
+                                const [deleteOpen, setDeleteOpen] = useState<boolean>(false);
+                                const [editOpen, setEditOpen] = useState<boolean>(false);
                                 return (
-                                    <div key={`event_${i}`}>
+                                    <div key={`event_${event.id}`}>
                                         <div className="py-2 pl-4 group hover:bg-secondary hover:rounded-lg transition-all flex justify-between items-center">
                                             <div>
                                                 <div className="text-md font-bold">{event.title}</div>
                                                 <div className="text-sm text-muted-foreground">{event.location}</div>
                                                 <div className="text-sm text-muted-foreground">{date}</div>
                                             </div>
-                                            <div className="flex flex-row items-center my-auto mr-8 w-20 h-8 gap-4">
+                                            <div className="flex flex-row items-center my-auto mr-8 h-8 gap-4 mr-4">
                                                 <Link href={`events-qr/${event.id}`}>
                                                     <MdOutlineQrCode2 className="hover:cursor-pointer h-6 w-6" />
                                                 </Link>
@@ -81,9 +82,9 @@ export function EventsListCard({ partialEvents, title, empty_message }: EventsLi
                                                 />
                                                 <PopupDialog
                                                     popupButton={<div><FaTrash className="hover:cursor-pointer hover:fill-destructive h-5 w-4" /></div>}
-                                                    title="Delete Event"
+                                                    title={`Delete Event (${event.title})`}
                                                     description="Are you sure you want to delete this event?"
-                                                    dialogContent={<DeleteConfirmationForm event={event} />}
+                                                    dialogContent={<DeleteConfirmationForm event={event} setDeleteOpen={setDeleteOpen} />}
                                                     open={deleteOpen}
                                                     setOpen={setDeleteOpen}
                                                 />
