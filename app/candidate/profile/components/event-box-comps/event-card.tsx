bb9486ec5f5bb925636ae06ccada89ddd2090852
TheDeafOne <Woodburnkb20@gcc.edu>
2024-04-13 15:12:03 -0400

    manage filtering candidate side events that've been deleted

--- app/candidate/profile/components/event-box-comps/event-card.tsx
+++ app/candidate/profile/components/event-box-comps/event-card.tsx
@@ -1,20 +1,25 @@
 import getData from "@/app/api/getData";
+import { Event } from "@/app/types/event";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { parseISO } from "date-fns";
 import { useEffect, useState } from "react";
-import { EventBox } from "./event-box";
 
 export function EventCard({ eventIds }: { eventIds: string[] }) {
-    const [events, setEvents] = useState<any[]>([])
+    const [events, setEvents] = useState<Event[]>([])
 
     useEffect(() => {
         async function fetchEvents() {
             const parsedEvents = await Promise.all(eventIds.map(async (id) => {
-                return await getData({ collection_name: "events", document_id: id })
+                const unparsedEvent = await getData({ collection_name: "events", document_id: id });
+                if (!unparsedEvent) return null;
+                unparsedEvent.date = parseISO(unparsedEvent.date);
+                return unparsedEvent
             }))
-            setEvents(parsedEvents as any[])
+            setEvents(parsedEvents.filter((event) => event !== null) as Event[])
         }
         fetchEvents()
     }, [eventIds])
+
     return (
         <Card className="min-h-full ">
             <CardHeader className="flex flex-row items-center divide-x border-b mb-4">
@@ -26,7 +31,12 @@ export function EventCard({ eventIds }: { eventIds: string[] }) {
 
                 {events.map((event, index) => {
                     return (
-                        <EventBox key={`event_${index}`} event={event} />
+                        <Card key={`event_${index}`} className="min-h-full w-full">
+                            <CardContent className="flex flex-col flex-wrap w-full pt-4">
+                                <p className="font-bold text-lg">{event.title}</p>
+                                <p className="text-sm">{event.date.toDateString()}</p>
+                            </CardContent>
+                        </Card>
                     )
                 })}
             </CardContent>
