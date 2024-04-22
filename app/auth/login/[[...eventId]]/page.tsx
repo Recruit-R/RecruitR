8ca78c96eee2881283c82bd0d4dc5cd33d240ad3
TheDeafOne <Woodburnkb20@gcc.edu>
2024-04-22 13:37:23 -0400

    logo

--- app/auth/login/[[...eventId]]/page.tsx
+++ app/auth/login/[[...eventId]]/page.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 
 import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import picture from "@/resources/assets/logo-v2.svg";
+import Image from "next/image";
 import { UserAuthForm } from "../../components/user-auth-form";
-
 export const metadata: Metadata = {
     title: "Authentication",
     description: "Authentication forms built using the components.",
